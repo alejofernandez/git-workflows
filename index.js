2af54b38610e290afd164119117c74882d5ef233
Alejo Fernandez <alejofernandez@gmail.com>
2019-04-16 08:31:10 -0300

    Feature 5

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const feature1 = require('./feature1');
 const feature2 = require('./feature2');
+const feature4 = require('./feature4');
 
 module.exports = {
   feature1,
-  feature2
+  feature2,
+  feature4
 }
