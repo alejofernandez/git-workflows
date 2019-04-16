58c590a596fad419784a9387a9ed2c459beaa4ce
Santiago Canti <santiago.canti@auth0.com>
2019-04-16 08:26:06 -0300

    feat: add feature 3

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const feature1 = require('./feature1');
 const feature2 = require('./feature2');
+const feature3 = require('./feature3');
 const feature4 = require('./feature4');
+// hey
 
 module.exports = {
   feature1,
   feature2,
+  feature3, // no there is no problem
   feature4
 }
